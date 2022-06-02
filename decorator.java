eeb78060e9b89ea4f355dcae1a1ff272ede327b2
Rafael Marinho <rafael.mgf2001@gmail.com>
2022-06-01 23:47:29 -0300

    refactor: change decorator example

---
+++ decorator.java
@@ -0,0 +1,83 @@
+public interface Coffee {
+    public double getCost();
+    public String getIngredients();
+}
+
+public class SimpleCoffee implements Coffee {
+    @Override
+    public double getCost() {
+        return 1;
+    }
+
+    @Override
+    public String getIngredients() {
+        return "Café";
+    }
+}
+
+public abstract class CoffeeDecorator implements Coffee {
+    private final Coffee decoratedCoffee;
+
+    public CoffeeDecorator(Coffee c) {
+        this.decoratedCoffee = c;
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedCoffee.getCost();
+    }
+
+    @Override
+    public String getIngredients() {
+        return decoratedCoffee.getIngredients();
+    }
+}
+
+class WithMilk extends CoffeeDecorator {
+    public WithMilk(Coffee c) {
+        super(c);
+    }
+
+    @Override
+    public double getCost() { // Overriding methods defined in the abstract superclass
+        return super.getCost() + 0.5;
+    }
+
+    @Override
+    public String getIngredients() {
+        return super.getIngredients() + ", Leite";
+    }
+}
+
+class WithCaramel extends CoffeeDecorator {
+    public WithCaramel(Coffee c) {
+        super(c);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 0.2;
+    }
+
+    @Override
+    public String getIngredients() {
+        return super.getIngredients() + ", Caramelo";
+    }
+}
+
+public class Main {
+    public static void printInfo(Coffee c) {
+        System.out.println("Custo: " + c.getCost() + "; Ingredientes: " + c.getIngredients());
+    }
+
+    public static void main(String[] args) {
+        Coffee c = new SimpleCoffee();
+        printInfo(c);
+
+        c = new WithMilk(c);
+        printInfo(c);
+
+        c = new WithCaramel(c);
+        printInfo(c);
+    }
+}
